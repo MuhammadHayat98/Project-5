2eec97823a7512f614322658226ce059c8617ef9
MuhammadHayat98 <muhammad.hayat.698@my.csun.edu>
2018-04-22 22:53:48 -0700

    t

---
+++ src/Driver.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashTableProblems t = new HashTableProblems();
+		int[] a = t.randomIntArray(20, 30, false);
+		System.out.print(Arrays.toString(a));
+	}
+
+}
---
+++ src/HashTableProblems.java
@@ -0,0 +1,51 @@
+import java.util.Hashtable;
+
+public class HashTableProblems {
+	
+	public static int[] randomIntArray(int n, int limit, boolean nodups) {
+		int[] array = new int[n];
+		if(nodups == false) {
+			for (int i = 0; i < array.length; i++) {
+				int num = (int)(Math.random() * ((limit-1) + 1)) + 1;
+				array[i] = num;
+			}
+		}
+		else {
+			int N = 101;
+			Hashtable<Integer, Integer> htable = new Hashtable<Integer, Integer>(N, 0.75f);
+			for(int i = 0; i < array.length; i++) {
+				
+				if(htable.containsKey(array[i])) {
+					htable.remove(array[i]);
+				}
+				else
+					htable.put(i, array[i]);
+			}
+		}
+		
+		return array;
+	}
+	
+	public static ArrayList<Integer> numbersInCommon(int[] list1, int[] list2) {
+		int N = 101;
+		Hashtable<String, Integer> inCommon = new Hashtable<String, Integer>(N, 0.75f);
+		
+	}
+	
+}
+
+
+
+class Tuple {
+	int x;
+	int y;
+	
+	public Tuple(int a, int b) {
+		x = a;
+		y = b;
+	}
+	
+	public String toString() {
+		return "(" + x + "," + y + ")";
+	}
+}
